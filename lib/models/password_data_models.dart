60e4d337651b028568444b627c05a257d0be59d4
IsaacColney <isaaclalpekhluacolney@gmail.com>
2022-12-03 15:08:22 +0530

    add sqflite database utility

--- lib/models/password_data_models.dart
+++ lib/models/password_data_models.dart
@@ -1,11 +1,50 @@
+const String passwordTable = "password";
+
+class PasswordField {
+  static const List<String> values = [
+    "id",
+    "siteName",
+    "url",
+    "userName",
+    "password",
+  ];
+
+  static const String id = "_id";
+  static const String siteName = "siteName";
+  static const String url = "url";
+  static const String userName = "userName";
+  static const String password = "password";
+}
+
 class PasswordDataModel {
+  final int? id;
   final String siteName;
   final String url;
   final String userName;
   final String password;
 
-  const PasswordDataModel(
-      this.siteName, this.url, this.password, this.userName);
+  const PasswordDataModel({
+    this.id,
+    required this.siteName,
+    required this.url,
+    required this.userName,
+    required this.password,
+  });
+
+  PasswordDataModel copy({
+    int? id,
+    String? siteName,
+    String? url,
+    String? userName,
+    String? password,
+  }) =>
+      PasswordDataModel(
+        id: id ?? this.id,
+        siteName: siteName ?? this.siteName,
+        url: url ?? this.url,
+        userName: userName ?? this.userName,
+        password: password ?? this.password,
+      );
 
   Map<String, String> toMap() {
     return {
@@ -15,6 +54,14 @@ class PasswordDataModel {
       "password": password
     };
   }
+
+  static PasswordDataModel fromJson(Map<String, Object?> json) =>
+      PasswordDataModel(
+          id: json[PasswordField.id] as int?,
+          siteName: json[PasswordField.siteName] as String,
+          url: json[PasswordField.url] as String,
+          userName: json[PasswordField.userName] as String,
+          password: json[PasswordField.password] as String);
 
   @override
   String toString() {
--- lib/utils/sqlite_utils.dart
+++ lib/utils/sqlite_utils.dart
@@ -1,1 +1,84 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:password_manager/models/password_data_models.dart';
 
+class SQLDatabaseUtils {
+  static final SQLDatabaseUtils instance = SQLDatabaseUtils._init();
+  static Database? _database;
+
+  SQLDatabaseUtils._init();
+
+  Future<Database> get database async {
+    if (_database != null) {
+      return _database!;
+    } else {
+      _database = await _initDB("password.db");
+      return _database!;
+    }
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final String dbPath = await getDatabasesPath();
+    final String path = join(dbPath, filePath);
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    const idType = 'INTEGER PRIMARY KEY AUTOINCREMENT';
+    const urlType = "TEXT";
+    const userNameType = "TEXT";
+    const passwordType = "TEXT";
+
+    await db.execute('''
+     CREATE TABLE $passwordTable (
+      ${PasswordField.id} $idType,
+      ${PasswordField.url} $urlType,
+      ${PasswordField.userName} $userNameType,
+      ${PasswordField.password} $passwordType
+
+     )''');
+  }
+
+  Future<PasswordDataModel> create(PasswordDataModel passwordData) async {
+    Database db = await instance.database;
+    int id = await db.insert(passwordTable, passwordData.toMap());
+    return passwordData.copy(id: id);
+  }
+
+  Future<PasswordDataModel> readPasswordBySiteName(String siteName) async {
+    Database db = await instance.database;
+    final map = await db.query(passwordTable,
+        columns: PasswordField.values,
+        where: '${PasswordField.siteName} = ?',
+        whereArgs: [siteName]);
+    if (map.isNotEmpty) {
+      return PasswordDataModel.fromJson(map.first);
+    } else {
+      throw Exception("Site Name $siteName not found!");
+    }
+  }
+
+  Future<List<PasswordDataModel>> readAllPassword() async {
+    Database db = await instance.database;
+    String orderBy = '${PasswordField.siteName} ASC';
+    final result = await db.query(passwordTable, orderBy: orderBy);
+    return result.map((json) => PasswordDataModel.fromJson(json)).toList();
+  }
+
+  Future<int> update(PasswordDataModel password) async {
+    Database db = await instance.database;
+    return db.update(passwordTable, password.toMap(),
+        where: '${PasswordField.id} = ?', whereArgs: [password.id]);
+  }
+
+  Future<int> delete(int id) async {
+    Database db = await instance.database;
+    return await db.delete(passwordTable,
+        where: '${PasswordField.id} = ?', whereArgs: [id]);
+  }
+
+  Future close() async {
+    final db = await instance.database;
+    db.close();
+  }
+}
